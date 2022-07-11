0848b559e7907dcf4ce7fab7975c60bd6debed97
poovarasan-k <poovarasankaliyamoorthy96@gmail.com>
2022-07-11 18:51:35 +0530

    first commit

---
+++ src/components/About/Education/timelineElements.js
@@ -0,0 +1,44 @@
+let timelineElements = [
+    {
+        id: 1,
+        title: "Searching a Job!",
+        subTitle: "Frontend Developer",
+        location: "Chennai, Tamil Nadu",
+        description:
+            "A front end developer has one general responsibility: to ensure that website visitors can easily interact with the page. They do this through the combination of design, technology and programming to code a website's appearance, as well as taking care of debugging.",
+        date: " - present",
+        icon: "job",
+    },
+    {
+        id: 2,
+        title: "Bachelor of Computer Application",
+        subTitle: "Jaya College Of Arts And Science",
+        location: "Thiruninravur, Tamil Nadu",
+        description:
+            "In my college life, along with study, I and my friends enjoyed a lot of other things. We travelled lots of places, had new experiences and learned many new things. Our college's canteen was the remarkable place of my college life as whenever we got time, we used to chill in the canteen.",
+        date: "2020 - 2017",
+        icon: "work",
+    },
+    {
+        id: 3,
+        title: "Higher Secondary Certificate",
+        subTitle: "ICF Higher Secondary School",
+        location: "Chennai, Tamil Nadu",
+        description:
+            "School life is one of the most important phases in the life of a man. It is the period during which one learns the basic principles of behavior, speech, and etiquette. School life, in fact, is a period of learning and training in all the aspects of the personality of a man.",
+        date: "2017 - 2015",
+        icon: "school",
+    },
+    {
+        id: 4,
+        title: "Secondary School Leaving Certificate",
+        subTitle: "ICF Higher Secondary School",
+        location: "Chennai, Tamil Nadu",
+        description:
+            "School life is one of the most important phases in the life of a man. It is the period during which one learns the basic principles of behavior, speech, and etiquette. School life, in fact, is a period of learning and training in all the aspects of the personality of a man.",
+        date: " 2015",
+        icon: "school",
+    },
+]
+
+export default timelineElements
